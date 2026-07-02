c362f4e1ca5c754fa317f374b7050e50d082e5aa
Vibe Kanban <noreply@vibekanban.com>
2026-07-02 19:35:46 +0800

    chore: cleanup repo - remove tmp e2e artifacts, compiled JS in src/, legacy contracts package, cert .bak files
    - Remove 14 .tmp-browser-e2e-*.json files (debug residuals, no runtime use)
    - Remove identity-access/src/**/*.js and *.js.map (compiled artifacts, runtime uses dist/)
    - Remove packages/contracts/ entirely (zero references in codebase, legacy package)
    - Remove docker/office-addin/certs/*.bak (stale backup files)
    - Update .gitignore to prevent recurrence of tmp json and compiled JS in src/

--- apps/backend/governance/identity-access/src/metadata/authz.constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROLES_KEY = exports.REQUIRED_PERMISSIONS_KEY = exports.SKIP_RBAC_KEY = exports.IS_PUBLIC_KEY = void 0;
-exports.IS_PUBLIC_KEY = 'isPublic';
-exports.SKIP_RBAC_KEY = 'skipRbac';
-exports.REQUIRED_PERMISSIONS_KEY = 'requiredPermissions';
-exports.ROLES_KEY = 'roles';
-//# sourceMappingURL=authz.constants.js.map
